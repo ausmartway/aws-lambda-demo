76ea899732b5992d24e181dd3ba90bb2be7082e9
Yulei Liu <yulei.liu@gmail.com>
2019-05-16 21:19:31 +1000

    Update api_gateway.tf

--- api_gateway.tf
+++ api_gateway.tf
@@ -53,3 +53,20 @@ resource "aws_api_gateway_deployment" "example" {
   rest_api_id = "${aws_api_gateway_rest_api.example.id}"
   stage_name  = "test"
 }
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.example.arn}"
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the API Gateway "REST API".
+  source_arn = "${aws_api_gateway_deployment.example.execution_arn}/*/*"
+}
+
+output "base_url" {
+  value = "${aws_api_gateway_deployment.example.invoke_url}"
+}
+
+
